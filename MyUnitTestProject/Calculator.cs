42b0f587a6c8bcccf23bee1a031040e021ef28a4
Travis_Stuart_Higgins <t.higgins@ihsmarkit.com>
2023-09-12 16:45:51 +0100

    Added multiply, subtract and divide functionality.

--- MyUnitTestProject/Calculator.cs
+++ MyUnitTestProject/Calculator.cs
@@ -4,4 +4,20 @@ public class Calculator
     {
         return a + b;
     }
+
+    public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+    public int Multiply(int a, int b)
+        {
+            return a * b
+        }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
 }
